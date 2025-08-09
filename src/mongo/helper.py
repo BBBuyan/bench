fcaae0b4d16da712c522c7d0f77c9418ecd18e0d
Batbuyan Buyandelger <batbuyan.buyandelger@stud.htwk-leipzig.de>
2025-08-09 21:58:41 +0200

    logger

--- src/mongo/helper.py
+++ src/mongo/helper.py
@@ -90,7 +90,7 @@ def generate_insert_batch(size: int, coll: Collection):
 
 def save_result(old: list[float], new: list[float], operation: str):
     with open("result.txt", "a") as f:
-        f.write(f"##{operation:<45}\n")
+        f.write(f"##{operation}\n")
         f.write(f"|{'depth':^10}|{'w/o index':^10}|{'w index':^10}|{'diff':^10}|\n")
         f.write(f"|{'':-^10}|{'':-^10}|{'':-^10}|{'':-^10}|\n")
         for i in range(len(old)):

0bc6f7e4023acb052ac59031a6b5374b82f3c765
Batbuyan Buyandelger <batbuyan.buyandelger@stud.htwk-leipzig.de>
2025-09-06 16:23:05 +0200

    description generator with help of hawki

---
+++ src/description_generator/conn.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+url = "https://hawki2.htwk-leipzig.de/api/ai-req"
+headers = {
+    "Authorization": f"Bearer {os.getenv("API_KEY")}",
+    "Content-Type": "application/json",
+}
+
+command = """
+Create 100 synthetic descrptions of a device in NDJSON format, only the description field. 
+Do NOT add ```json ``` or any code block. Output plain NDJSON, one JSON object per line, like:
+{"description": "first description"}
+{"description": "second description"}
+"""
+
+my_data = {
+    "payload": {
+        "model": "gpt-4o",
+        "messages": [
+            {
+                "role": "user",
+                "content": {
+                    "text": command
+                }
+            }
+        ]
+    }
+}

7e3dbd94266c67c684aa03ed56fa00422a2b3ccd
Batbuyan Buyandelger <bbuyan5@gmail.com>
2025-09-28 15:37:02 +0000

    fix

--- src/mongo/__main__.py
+++ src/mongo/__main__.py
@@ -4,6 +4,6 @@ from .mongo_bench_client import MongoBench
 if __name__ == "__main__":
     bench = MongoBench()
 
-    # bench.bench_flat()
+    bench.bench_flat()
     bench.bench_obj()
-    # bench.bench_arr()
+    bench.bench_arr()

978ba47809156f57496c81477a77169fe6d5e76d
Batbuyan Buyandelger <batbuyan.buyandelger@stud.htwk-leipzig.de>
2025-09-28 17:36:19 +0200

    fetch

--- src/mongo/mongo_helper.py
+++ src/mongo/mongo_helper.py
@@ -5,12 +5,8 @@ from .mongo_types import BaseMongo
 
 def fetch_data_from_file(type: BaseMongo):
     path = Path(__file__).parent.parent.parent/"data"/f"{type.name}.json"
-    offset = randint(0, type.max_docs-type.fetch_limit)
     data =[]
     with open(path, "r") as f:
-        for _ in range(offset):
-            next(f, None)
-
         i = 0
         for line in f:
             json_data = loads(line)
@@ -18,12 +14,14 @@ def fetch_data_from_file(type: BaseMongo):
             i += 1
             if i >= type.fetch_limit:
                 break
-
     return data
 
 def create_indexes(types: list[BaseMongo]):
+    print("---CREATING INDEXES")
     for type in types:
+        print(f"{type.name}", end=" ", flush=True)
         type.coll.create_index(type.error_path)
+    print("DONE---")
 
 def drop_indexes(types: list[BaseMongo]):
     for type in types:

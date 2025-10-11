336ef05b6362d9fecdfc5e76b53eb27658a17b2b
Batbuyan Buyandelger <bbuya5@gmail.com>
2025-10-11 16:29:57 +0200

    log

--- src/mongo/mongo_logger.py
+++ src/mongo/mongo_logger.py
@@ -1,19 +1,18 @@
-from pathlib import Path
 from datetime import datetime
 
 suffix = "insert_unordered"
 
 def save_result(durations: list[float], operation: str, coll_type: str):
-    file_name = f"{coll_type}_{suffix}.txt"
-    file_path = Path(__file__).parent.parent.parent/"result"/"mongodb"/file_name
+    file_name = f"{coll_type}.txt"
+    file_path = f"result/{file_name}"
     rounded = [round(d) for d in durations]
 
     with open(file_path, "a") as f:
         f.write(f"{operation:<20}: {str(rounded)}\n")
 
 def mark_operation(coll_type: str):
-    file_name = f"{coll_type}_{suffix}.txt"
-    file_path = Path(__file__).parent.parent.parent/"result"/"mongodb"/file_name
+    file_name = f"{coll_type}.txt"
+    file_path = f"result/{file_name}"
     now = datetime.now()
     now_date = now.strftime("%Y-%m-%d, %H:%M:%S")
     with open(file_path, "a") as f:
@@ -22,7 +21,7 @@ def mark_operation(coll_type: str):
 
 def mark_message(coll_type: str, msg: str):
     file_name = f"{coll_type}_{suffix}.txt"
-    file_path = Path(__file__).parent.parent.parent/"result"/"mongodb"/file_name
+    file_path = f"result/{file_name}"
     with open(file_path, "a") as f:
         f.write(msg + "\n")
 

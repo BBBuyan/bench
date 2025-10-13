38cfc54baf7f6d0197943c81703e11801faec50c
Batbuyan Buyandelger <bbuya5@gmail.com>
2025-10-13 20:44:36 +0200

    warmup

--- src/elastic/es_workloads.py
+++ src/elastic/es_workloads.py
@@ -9,12 +9,19 @@ def run_read_only(types: list[Base]):
     result = [0.0] * len(types)
 
     for i in range(len(types)):
+        ### warmup ###
+        for _ in range(3):
+            t = op.time_memory_read(types[i])
+            print(f"{round(t)}, ", end="", flush=True)
+
         print(f"depth: {types[i].name} | ", end=" ", flush=True)
-        op.time_memory_read(types[i])
+        type_results = []
         for _ in range(num_of_tries):
             print("r,", end=" ", flush=True)
-            result[i] += op.time_memory_read(types[i])
+            type_results.append(op.time_memory_read(types[i]))
         print("---")
+        print([f"{x:.1f}" for x in type_results])
+        result[i] = sum(type_results)/len(type_results)
 
     logger.save_result(result, "read model_nr", types[0].coll_type)
     return result
@@ -24,12 +31,20 @@ def run_avg_only(types: list[Base]):
     result = [0.0] * len(types)
 
     for i in range(len(types)):
+        ### warmup ###
+        for _ in range(3):
+            t = op.time_avg(types[i])
+            print(f"{round(t)}, ", end="", flush=True)
+
+
         print(f"depth: {types[i].name} | ", end=" ", flush=True)
-        op.time_avg(types[i])
+        type_results = []
         for _ in range(num_of_tries):
             print("a,", end=" ", flush=True)
-            result[i] += op.time_avg(types[i])
+            type_results.append(op.time_avg(types[i]))
         print("---")
+        print([f"{x:.1f}" for x in type_results])
+        result[i] = sum(type_results)/len(type_results)
 
     logger.save_result(result, "avg", types[0].coll_type)
     return result
@@ -39,12 +54,21 @@ def run_group_only(types: list[Base]):
     result = [0.0] * len(types)
 
     for i in range(len(types)):
+        ### warmup ###
+        for _ in range(3):
+            t = op.time_group(types[i])
+            print(f"{round(t)}, ", end="", flush=True)
+
+
+
         print(f"depth: {types[i].name} | ", end=" ", flush=True)
-        op.time_group(types[i])
+        type_results = []
         for _ in range(num_of_tries):
             print("g,", end=" ", flush=True)
-            result[i] += op.time_group(types[i])
+            type_results.append(op.time_group(types[i]))
         print("---")
+        print([f"{x:.1f}" for x in type_results])
+        result[i] = sum(type_results)/len(type_results)
 
     logger.save_result(result, "group", types[0].coll_type)
     return result
@@ -54,12 +78,20 @@ def run_info_search(types: list[Base]):
     result = [0.0] * len(types)
 
     for i in range(len(types)):
+        ### warmup ###
+        for _ in range(3):
+            t = op.time_info_read(types[i])
+            print(f"{round(t)}, ", end="", flush=True)
+
+        
         print(f"depth: {types[i].name} | ", end=" ", flush=True)
-        op.time_info_read(types[i])
+        type_results = []
         for _ in range(num_of_tries):
             print("g,", end=" ", flush=True)
-            result[i] += op.time_info_read(types[i])
+            type_results.append(op.time_info_read(types[i]))
         print("---")
+        print([f"{x:.1f}" for x in type_results])
+        result[i] = sum(type_results)/len(type_results)
 
     logger.save_result(result, "info", types[0].coll_type)
     return result
@@ -69,12 +101,22 @@ def run_description_search(types: list[Base]):
     result = [0.0] * len(types)
 
     for i in range(len(types)):
+        ### warmup ###
+        for _ in range(3):
+            t = op.time_description_read(types[i])
+            print(f"{round(t)}, ", end="", flush=True)
+
+
+
         print(f"depth: {types[i].name} | ", end=" ", flush=True)
-        op.time_description_read(types[i])
+        type_results = []
         for _ in range(num_of_tries):
             print("g,", end=" ", flush=True)
-            result[i] += op.time_description_read(types[i])
+            type_results.append(op.time_description_read(types[i]))
         print("---")
+
+        print([f"{x:.1f}" for x in type_results])
+        result[i] = sum(type_results)/len(type_results)
 
     logger.save_result(result, "description", types[0].coll_type)
     return result
